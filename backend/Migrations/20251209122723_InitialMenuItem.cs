c24a2e4309e7a6851a9cea34466cfdb85c9480bb
reineeeeeeee <jlhanrossepferrer@iskolarngbayan.pup.edu.ph>
2025-12-09 20:40:30 +0800

    backend

--- backend/Migrations/20251209122723_InitialMenuItem.cs
+++ backend/Migrations/20251209122723_InitialMenuItem.cs
@@ -5,7 +5,7 @@
 namespace backend.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class InitialMenuItem : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
